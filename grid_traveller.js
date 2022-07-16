c03684a6ae131ecdfc125a57dcba5fa91b123fe6
koomekevo <koome.kevo@gurucrafts.net>
2022-07-16 16:41:25 +0300

    Fix range error

--- grid_traveller.js
+++ grid_traveller.js
@@ -1,7 +1,7 @@
 const grid_traveller = (m, n) => {
     if (m === 1 && n === 1) return 1;
-    if (m === 0 && n === 0) return 0;
-    return grid_traveller(m - 1, n) + (n - 1, m);
+    if (m === 0 || n === 0) return 0;
+    return grid_traveller(m - 1, n) + grid_traveller(n - 1, m);
 };
 
 console.log(grid_traveller(1, 1));
