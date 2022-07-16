459605ed4b86776db7fad278e86f6f5392f18525
koomekevo <koome.kevo@gurucrafts.net>
2022-07-16 16:10:21 +0300

    Memoize fib.js to be efficient

--- fib.js
+++ fib.js
@@ -1,8 +1,10 @@
-const fib = (n) => {
+const fib = (n, memo = {}) => {
+    if (n in memo) return memo[n];
     if (n <= 2) return 1;
-    return fib(n - 1) + fib(n - 2);
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n]
 };
 
 console.log(fib(6));
 console.log(fib(7));
-console.log(fib(8));
+console.log(fib(50));
