628cde9638f49e4807e149c132bde929964212c8
koomekevo <koome.kevo@gurucrafts.net>
2022-07-20 16:43:04 +0300

    Add this.remove method

--- my_set.js
+++ my_set.js
@@ -6,15 +6,25 @@ function mySet() {
     return collection.indexOf(element) !== -1;
   };
 
+  // This will return all the values in the set
   this.values = () => {
     return collection;
   };
 
+  // This will add an element to the set
   this.add = (element) => {
-    if (!collection.has(element)) {
+    if (!this.has(element)) {
       collection.push(element);
       return true;
     }
     return false;
   };
+
+  this.remove = (element) => {
+    if (this.has(element)) {
+        index = collection.indexOf(element);
+        collection.splice(index, 1);
+        return true;
+    }
+  }
 }
