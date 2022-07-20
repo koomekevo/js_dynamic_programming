78cb4f8fc510b314abaad54a80e6e3baa9292d3a
koomekevo <koome.kevo@gurucrafts.net>
2022-07-20 15:49:59 +0300

    Add Stack class in stack.js

--- stack.js
+++ stack.js
@@ -17,7 +17,40 @@ for (let i = 0; i < word.length; i++) {
 }
 
 if (rword === word) {
-    console.log(word + "is a palindrome")
+  console.log(word + " is a palindrome");
 } else {
-    console.log(word + "is not a palindrome")
+  console.log(word + " is not a palindrome");
 }
+
+var Stack = function () {
+  this.count = 0;
+  this.storage = {};
+
+  this.push = (value) => {
+    this.storage[this.count] = value;
+    this.count++;
+  };
+
+  this.pop = () => {
+    if (this.count == 0) {
+      return undefined;
+    }
+
+    this.count--;
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  };
+
+  this.peek = () => {
+    return this.storage[this.count - 1];
+  };
+};
+
+var myStack = new Stack();
+
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
