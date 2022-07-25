55f11cbfe28601f4e1518e8bb653faaffdbac56e
koomekevo <koome.kevo@gurucrafts.net>
2022-07-25 15:22:38 +0300

    Add this.subset method

--- my_set.js
+++ my_set.js
@@ -69,6 +69,9 @@ function mySet() {
   }
 
   this.subset = (otherSet) => {
-    
-  }
+    var firstSet = this.values();
+    return firstSet.every(function (value) {
+      return otherSet.has(value);
+    });
+  };
 }
